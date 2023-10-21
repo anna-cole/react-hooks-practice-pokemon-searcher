29859d423fb33720c29a4f251a0c0305d9d43667
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-20 23:05:27 -0500

    Render pokemons working

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,11 +2,14 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemons }) {
   return (
-    <Card.Group itemsPerRow={6}>
+    <>
       <h1>Hello From Pokemon Collection</h1>
-    </Card.Group>
+      <Card.Group itemsPerRow={6} onClick={() => console.log('hi')} >
+        {pokemons.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} />)}
+      </Card.Group>
+    </>
   );
 }
 
